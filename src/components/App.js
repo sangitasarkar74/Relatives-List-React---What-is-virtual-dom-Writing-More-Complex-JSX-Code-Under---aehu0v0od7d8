88f6ffed1f0907c8c693ab531dfd386d5c84fabb
sangitasarkar74 <sangitasarkar@gmail.com>
2022-11-28 22:42:51 +0530

    Stage one

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,25 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import '../styles/App.css';
 
 class App extends Component {
-    render() {
+    render = () => {
+        const relative = [
+            { name: 'Tina', id: 'relativeListItem1' },
+            { name: 'Mina', id: 'relativeListItem2' },
+            { name: 'Sima', id: 'relativeListItem3 ' }
+        ];
 
-        return(
+        const relativeList = relative.map((relativeName) => {
+            return <li key={relativeName.id}>{relativeName.name}</li>
+        })
+
+        return (
             <div id="main">
-               {/* Do not remove the main div */}
+                {/* Do not remove the main div */
+                    <ol>
+                        {relativeList}
+                    </ol>
+                }
             </div>
         )
     }
